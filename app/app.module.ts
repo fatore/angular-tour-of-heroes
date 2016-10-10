4319ade7adbb9a69f152c0b1596f4acdea8e3f79
Francisco Fatore <francisco.fatore@realimpactanalytics.com>
2016-10-10 09:14:12 +0200

    Refactor heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,24 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
     FormsModule
   ],
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
+  ],
+  providers: [
+      HeroService
   ],
   bootstrap: [ AppComponent ]
 })
 
-export class AppModule { }
+export class AppModule {}
