b19e0dd802428d1791f7bf1420c3f226d7f8c97c
Francisco Fatore <francisco.fatore@realimpactanalytics.com>
2016-10-12 21:28:57 +0200

    Update and create

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,11 +7,16 @@ import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
 import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
+import {HttpModule} from '@angular/http';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: '',
